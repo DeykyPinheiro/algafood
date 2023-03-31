b49b6156a6948ce4242bf71eca0006aab97bce3e
Deyky Pinheiro <pinheiromikael96@gmail.com>
2023-03-31 20:08:56 -0300

    cobertura de test em CidadesServiceTest

---
+++ src/test/java/com/apigaworks/algafood/domain/service/CidadeServiceTest.java
@@ -0,0 +1,92 @@
+package com.apigaworks.algafood.domain.service;
+
+import com.apigaworks.algafood.domain.model.Cidade;
+import com.apigaworks.algafood.domain.model.Estado;
+import com.apigaworks.algafood.domain.repository.CidadeRepository;
+import org.junit.jupiter.api.*;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ActiveProfiles("test")
+class CidadeServiceTest {
+
+    @MockBean
+    private CidadeRepository cidadeRepository;
+
+    @Autowired
+    private CidadeService cidadeService =  new CidadeService(cidadeRepository);
+
+    private List<Cidade> listaCidades = new ArrayList<>();
+    private Cidade cidade;
+    private Estado estado;
+
+    private Long id;
+
+    @BeforeEach
+    public void setUp() {
+        startValues();
+        startMocks();
+    }
+
+    @Test
+    @DisplayName("salvar cidade")
+    void testSalvarCidade() {
+        Cidade c = cidadeService.salvar(this.cidade);
+        Assertions.assertEquals(this.cidade, c);
+        Assertions.assertEquals(this.cidade.getClass(), c.getClass());
+        Mockito.verify(cidadeRepository).salvar(this.cidade);
+    }
+
+    @Test
+    @DisplayName("buscar por id")
+    void testBuscarPorId() {
+        Cidade c = cidadeService.buscarPorId(this.id);
+        Assertions.assertEquals(this.cidade.getClass(), c.getClass());
+        Assertions.assertEquals(this.id, c.getId());
+    }
+
+    @Test
+    @DisplayName("listar cidades")
+    void listar() {
+        List<Cidade> listaCidades = cidadeService.listar();
+        Mockito.verify(cidadeRepository).listar();
+        Assertions.assertEquals(this.listaCidades.getClass(), listaCidades.getClass());
+        Assertions.assertEquals(this.listaCidades.size(), listaCidades.size());
+    }
+
+    @Test
+    @DisplayName("remover cidade")
+    void remover() {
+        cidadeService.remover(this.id);
+        Mockito.verify(cidadeRepository).remover(this.cidade);
+    }
+
+    private void startValues() {
+        this.id = 1L;
+        this.estado = new Estado(1l);
+        this.cidade = new Cidade(1L, "teste", estado);
+
+        listaCidades.add(this.cidade);
+        listaCidades.add(this.cidade);
+        listaCidades.add(this.cidade);
+        listaCidades.add(this.cidade);
+    }
+
+    private void startMocks() {
+        Mockito.when(cidadeRepository.salvar(cidade)).thenReturn(cidade);
+        Mockito.when(cidadeRepository.buscar(this.id)).thenReturn(cidade);
+        Mockito.when(cidadeRepository.listar()).thenReturn(this.listaCidades);
+    }
+
+}

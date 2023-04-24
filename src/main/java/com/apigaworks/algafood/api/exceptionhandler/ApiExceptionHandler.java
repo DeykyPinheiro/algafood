baa87f1d1cef7ff9056deeab841baa08f5854d52
Deyky Pinheiro <pinheiromikael96@gmail.com>
2023-04-24 14:50:44 -0300

    herdando excepitons padroes do spring

--- src/main/java/com/apigaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/apigaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -8,12 +8,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 
 //a anotacao ControllerAdvice captura as excecoes de forma global
+//estou herdadndo ResponseEntityExceptionHandler que é uma implementacao
+// padrao que ja trata as excepections do spring
 @ControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler  extends ResponseEntityExceptionHandler  {
 
     @ExceptionHandler(EntidadeNaoEncontradaException.class)
     public ResponseEntity<?> tratarEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e) {
@@ -39,18 +42,18 @@ public class ApiExceptionHandler {
                 .body(problema);
     }
 
-//    midias nao suportadas
-    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
-    public ResponseEntity<?> tratarHttpMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException e){
-
-        Problema problema = Problema.builder()
-                .dataHora(LocalDateTime.now())
-                .mensagem("tipo de midia nao é aceito, tente json").build();
-
-
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(problema);
-    }
+////    midias nao suportadas
+//    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+//    public ResponseEntity<?> tratarHttpMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException e){
+//
+//        Problema problema = Problema.builder()
+//                .dataHora(LocalDateTime.now())
+//                .mensagem("tipo de midia nao é aceito, tente json").build();
+//
+//
+//        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+//                .body(problema);
+//    }
 
     @ExceptionHandler(EntidadeEmUsoException.class)
     public ResponseEntity<?> tratarEntidadeEmUsoException(EntidadeEmUsoException e){

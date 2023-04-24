7d6c32844e5a3ae96ad653287bc362259fd9bc72
Deyky Pinheiro <pinheiromikael96@gmail.com>
2023-04-24 11:19:03 -0300

    aumentando a granularidade das exception

--- src/main/java/com/apigaworks/algafood/api/controller/CidadeController.java
+++ src/main/java/com/apigaworks/algafood/api/controller/CidadeController.java
@@ -1,9 +1,13 @@
 package com.apigaworks.algafood.api.controller;
 
+import com.apigaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.apigaworks.algafood.domain.exception.EstadoNaoEncontradoException;
+import com.apigaworks.algafood.domain.exception.NegocioException;
 import com.apigaworks.algafood.domain.model.Cidade;
-import com.apigaworks.algafood.domain.model.Cozinha;
 import com.apigaworks.algafood.domain.service.CidadeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -31,7 +35,7 @@ public class CidadeController {
         return cidadeService.salvar(cidade);
     }
 
-//    padrao na hora de atualizar é receber o id no path e os
+    //    padrao na hora de atualizar é receber o id no path e os
 //    dados a serem atualizados todos no body
     @PutMapping("/{id}")
     public Cidade atualizar(@PathVariable Long id, @RequestBody Cidade cidade) {
@@ -43,4 +47,22 @@ public class CidadeController {
     public void excluir(@PathVariable Long id) {
         cidadeService.remover(id);
     }
+
+
+    //    tratando exception dentro do controlador, apenas requisicoes ref
+    //    a cidadeController
+    @ExceptionHandler(EntidadeNaoEncontradaException.class)
+    public ResponseEntity<?> tratarEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(e.getMessage());
+    }
+
+//    tratamento especifico de negocio
+    @ExceptionHandler(NegocioException.class)
+    public  ResponseEntity<?> tratarNegocioException(NegocioException e){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(e.getMessage());
+    }
+
+
 }

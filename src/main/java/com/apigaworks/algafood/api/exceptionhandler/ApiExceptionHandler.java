b28bcdfb06c98b3274ccf70e80e3a36e120a0cc4
Deyky Pinheiro <pinheiromikael96@gmail.com>
2023-04-25 10:51:15 -0300

    padronizando sainda de exceptions inclusive as do spring

--- src/main/java/com/apigaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/apigaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -3,11 +3,14 @@ package com.apigaworks.algafood.api.exceptionhandler;
 import com.apigaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.apigaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.apigaworks.algafood.domain.exception.NegocioException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
@@ -16,30 +19,22 @@ import java.time.LocalDateTime;
 //estou herdadndo ResponseEntityExceptionHandler que é uma implementacao
 // padrao que ja trata as excepections do spring
 @ControllerAdvice
-public class ApiExceptionHandler  extends ResponseEntityExceptionHandler  {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
+//    spring passa uma instancia da web request, o resto pode ser instanciar e modificado para passar a resposta
+//    dessa forma toda e qualquer excessao na hora de estourar passa por  handleExceptionInternal, inclusive as do spring
     @ExceptionHandler(EntidadeNaoEncontradaException.class)
-    public ResponseEntity<?> tratarEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e) {
+    public ResponseEntity<?> tratarEntidadeNaoEncontradaException(EntidadeNaoEncontradaException ex,  WebRequest request) {
 
-        Problema problema = Problema.builder()
-                .dataHora(LocalDateTime.now())
-                .mensagem(e.getMessage()).build();
-
-
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(problema);
+        HttpHeaders header  = new HttpHeaders();
+        return handleExceptionInternal(ex, ex.getMessage(), header, HttpStatus.NOT_FOUND, request);
     }
 
     @ExceptionHandler(NegocioException.class)
-    public ResponseEntity<?> tratarNegocioException(NegocioException e) {
+    public ResponseEntity<?> tratarNegocioException(NegocioException ex, WebRequest request){
 
-        Problema problema = Problema.builder()
-                .dataHora(LocalDateTime.now())
-                .mensagem(e.getMessage()).build();
-
-
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(problema);
+        HttpHeaders header  = new HttpHeaders();
+        return handleExceptionInternal(ex, ex.getMessage(), header, HttpStatus.NOT_FOUND, request);
     }
 
 ////    midias nao suportadas
@@ -56,14 +51,29 @@ public class ApiExceptionHandler  extends ResponseEntityExceptionHandler  {
 //    }
 
     @ExceptionHandler(EntidadeEmUsoException.class)
-    public ResponseEntity<?> tratarEntidadeEmUsoException(EntidadeEmUsoException e){
+    public ResponseEntity<?> tratarEntidadeEmUsoException(EntidadeEmUsoException ex, WebRequest request) {
 
-        Problema problema = Problema.builder()
-                .dataHora(LocalDateTime.now())
-                .mensagem("entidade em uso").build();
+        HttpHeaders header  = new HttpHeaders();
+        return handleExceptionInternal(ex, ex.getMessage(), header, HttpStatus.CONFLICT, request);
+    }
 
+    //    especializando metodo, colocando um corpo padrao para a resposta de erro ja definidas pelo spring
+    //    usado no erro application/xml quando a midia nao é suportada, e em outros erros
+    //    todas a exceptions do spring chamam ResponseEntityExceptionHandler, tal como eu faco com a negocioException
+    //    entao todas as excessoes estao cobertas
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatusCode statusCode, WebRequest request) {
 
-        return ResponseEntity.status(HttpStatus.CONFLICT)
-                .body(problema);
+        if (body == null) {
+            body = Problema.builder()
+                    .dataHora(LocalDateTime.now())
+                    .mensagem(ex.getMessage()).build();
+        } else if (body instanceof String) {
+            body = Problema.builder()
+                    .dataHora(LocalDateTime.now())
+                    .mensagem(ex.getMessage()).build();
+        }
+
+        return super.handleExceptionInternal(ex, body, headers, statusCode, request);
     }
 }

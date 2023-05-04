647c82a19580c7871bb19fa6d8a02419f64dd3ae
Deyky Pinheiro <pinheiromikael96@gmail.com>
2023-05-04 13:16:09 -0300

    simplificando teste

--- src/test/java/com/apigaworks/algafood/api/controller/CozinhaControllerTestIT.java
+++ src/test/java/com/apigaworks/algafood/api/controller/CozinhaControllerTestIT.java
@@ -9,6 +9,7 @@ import org.hamcrest.Matcher;
 
 import static org.hamcrest.Matchers.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -25,41 +26,71 @@ class CozinhaControllerTestIT {
     @LocalServerPort
     private int port;
 
+
+    @BeforeEach
+    public void setUp() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        RestAssured.port = port;
+        RestAssured.basePath = "/cozinhas";
+    }
+
     @Test
     void deveRetornar200_QuandoConsultarCozinhas() {
-
-//        isso habilita o log da resposta errada
-//        dai mostra exatamente o que foi feito e a resposta tbm
-        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
-
-//        quando acessar o endpoit, na porta, aceitando json
-//        usado get
-//        deverá retornar 200 status code
         given()
-                .basePath("/cozinhas")
-                .port(port)
                 .accept(ContentType.JSON)
-                .when()
+        .when()
                 .get()
-                .then()
+        .then()
                 .statusCode(HttpStatus.OK.value());
     }
 
     @Test
     void deveRetornar4Cozinhas_QuandoConsultarCozinhas() {
-        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
-
-//        verificar se tenho 4 cozinhas
-//        verificar se tenho os item indiana e tailandesa no campo de nome
-
         given()
-                .basePath("/cozinhas")
-                .port(port)
                 .accept(ContentType.JSON)
-                .when()
+        .when()
                 .get()
-                .then()
+        .then()
                 .body("", hasSize(4))
                 .body("nome", hasItems("Indiana", "Tailandesa"));
     }
 }
+
+//  MESMAS CLASSES DE CIMA ESCRITA DE UMA OUTRA FORMA, UM POUCO MAIS SIMPLES
+//    @Test
+//    void deveRetornar200_QuandoConsultarCozinhas() {
+//
+////        isso habilita o log da resposta errada
+////        dai mostra exatamente o que foi feito e a resposta tbm
+//        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+//
+////        quando acessar o endpoit, na porta, aceitando json
+////        usado get
+////        deverá retornar 200 status code
+//        given()
+//                .basePath("/cozinhas")
+//                .port(port)
+//                .accept(ContentType.JSON)
+//                .when()
+//                .get()
+//                .then()
+//                .statusCode(HttpStatus.OK.value());
+//    }
+//
+//    @Test
+//    void deveRetornar4Cozinhas_QuandoConsultarCozinhas() {
+//        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+//
+////        verificar se tenho 4 cozinhas
+////        verificar se tenho os item indiana e tailandesa no campo de nome
+//
+//        given()
+//                .basePath("/cozinhas")
+//                .port(port)
+//                .accept(ContentType.JSON)
+//                .when()
+//                .get()
+//                .then()
+//                .body("", hasSize(4))
+//                .body("nome", hasItems("Indiana", "Tailandesa"));
+//    }

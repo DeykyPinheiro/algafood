086cdc39978ed6a5cca9a7d35c0f9defa70a6110
Deyky Pinheiro <pinheiromikael96@gmail.com>
2023-05-05 11:05:45 -0300

    salvando e exibindo no padrao UTC

--- src/main/java/com/apigaworks/algafood/AlgafoodApplication.java
+++ src/main/java/com/apigaworks/algafood/AlgafoodApplication.java
@@ -1,6 +1,9 @@
 package com.apigaworks.algafood;
 
 import com.apigaworks.algafood.infrastructure.CustomJpaRepositoryImpl;
+
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -9,7 +12,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(repositoryBaseClass = CustomJpaRepositoryImpl.class)
 public class AlgafoodApplication {
 
+
+
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         SpringApplication.run(AlgafoodApplication.class, args);
     }
 

b5a7251697cdae0311ce3e8c4856319b6340a5a4
Deyky Pinheiro <pinheiromikael96@gmail.com>
2023-05-05 14:58:13 -0300

    removendo modelmapper e usando record

--- src/main/java/com/apigaworks/algafood/domain/dto/cozinha/CozinhaDTO.java
+++ src/main/java/com/apigaworks/algafood/domain/dto/cozinha/CozinhaDTO.java
@@ -4,16 +4,9 @@ import com.apigaworks.algafood.domain.model.Cozinha;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 
-public class CozinhaDTO {
-
-    @Autowired
-    private ModelMapper modelMapper;
-
-    private Long id;
-    private String nome;
-
+public record CozinhaDTO( Long id, String nome) {
     public CozinhaDTO(Cozinha data) {
-        // estou chamando cada propriedade e passando o valor pra ela
-        modelMapper.map(data, this);
+        this(data.getId(), data.getNome());
     }
 }
+
--- src/main/java/com/apigaworks/algafood/domain/dto/restaurante/RestauranteDTO.java
+++ src/main/java/com/apigaworks/algafood/domain/dto/restaurante/RestauranteDTO.java
@@ -2,27 +2,16 @@ package com.apigaworks.algafood.domain.dto.restaurante;
 
 import com.apigaworks.algafood.domain.dto.cozinha.CozinhaDTO;
 import com.apigaworks.algafood.domain.model.Restaurante;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.math.BigDecimal;
 
-public class RestauranteDTO {
-
-    @Autowired
-    private ModelMapper modelMapper;
-
-   private Long id;
-
-    private String nome;
-
-    private BigDecimal taxaFrete;
-
-    private CozinhaDTO cozinha;
-
+public record RestauranteDTO(Long id, String nome, BigDecimal taxaFrete, CozinhaDTO cozinha) {
 
 
     public RestauranteDTO(Restaurante data) {
-        modelMapper.map(data, this);
+        this(data.getId(), data.getNome(), data.getTaxaFrete(), new CozinhaDTO(data.getCozinha()));
     }
 }

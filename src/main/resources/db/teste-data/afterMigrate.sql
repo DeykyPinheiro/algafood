c174bc8e6adeea8406c5e4321ad9d282ec1cd0b2
Deyky Pinheiro <pinheiromikael96@gmail.com>
2023-05-12 13:25:13 -0300

    adicionando dados no afterMigrate

--- src/main/resources/db/teste-data/afterMigrate.sql
+++ src/main/resources/db/teste-data/afterMigrate.sql
@@ -72,3 +72,8 @@ insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('San
 insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Espetinho de Cupim', 'Acompanha farinha, mandioca e vinagrete', 8, 1, 6);
 
 insert into grupo (nome) values ('Gerente'), ('Vendedor'), ('Secretária'), ('Cadastrador');
+
+insert into usuario (id, nome, email, senha, data_cadastro) values (1, 'João da Silva', 'joao.ger@algafood.com', '123', utc_timestamp);
+insert into usuario (id, nome, email, senha, data_cadastro) values (2, 'Maria Joaquina', 'maria.vnd@algafood.com', '123', utc_timestamp);
+insert into usuario (id, nome, email, senha, data_cadastro) values (3, 'José Souza', 'jose.aux@algafood.com', '123', utc_timestamp);
+insert into usuario (id, nome, email, senha, data_cadastro) values (4, 'Sebastião Martins', 'sebastiao.cad@algafood.com', '123', utc_timestamp);

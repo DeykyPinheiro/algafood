f332c7c6d75f6d551da5ffbd4ce4dffa43010224
Deyky Pinheiro <pinheiromikael96@gmail.com>
2023-06-07 12:29:42 -0300

    mapeando fotoProduto em Produto

--- src/main/java/com/apigaworks/algafood/domain/model/FotoProduto.java
+++ src/main/java/com/apigaworks/algafood/domain/model/FotoProduto.java
@@ -1,11 +1,21 @@
 package com.apigaworks.algafood.domain.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.Data;
 
 @Embeddable
 @Data
 public class FotoProduto {
+
+    @Column(name = "nome_arquivo_foto")
+    private String nomeArquivo = "sem arquivo";
+
+    @Column(name = "content_type_foto")
+    private String contentType = "sem arquivo";
+
+    @Column(name = "tamanho_foto")
+    private Long tamanho = 0L;
 
 
 }
